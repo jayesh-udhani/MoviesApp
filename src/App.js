1781a933246b960c447c09aae0c7c56abbeb9c33
Jayesh <jayesh@shipmnts.com>
2018-09-02 23:00:05 +0530

    Trying to show data in table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Table } from "antd";
+const { Column } = Table;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movies: [] };
+  }
+  componentDidMount() {
+    const url = "http://starlord.hackerearth.com/movieslisting";
+    axios
+      .get(url)
+      .then(response => {
+        //console.log(response.data);
+        this.setState({ movies: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    console.log("STATE ", this.state.movies);
+    const { movies } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Table dataSource={movies} rowKey="movie_title">
+          <Column title="Movie" dataIndex="movie" />
+          <Column title="Director" dataIndex="director" />
+          <Column title="Actor 1" dataIndex="actor1" />
+          <Column title="Actor 2" dataIndex="actor2" />
+          <Column title="Genre" dataIndex="genre" />
+          <Column title="Language" dataIndex="language" />
+          <Column title="Country" dataIndex="country" />
+          <Column title="Content Rating" dataIndex="rating" />
+          <Column title="Budget" dataIndex="budget" />
+          <Column title="Year" dataIndex="year" />
+          <Column title="Plot Keywords" dataIndex="plot" />
+          <Column title="IMDB Link" dataIndex="link" />
+        </Table>
       </div>
     );
   }
