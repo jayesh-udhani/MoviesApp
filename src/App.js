9c73001a88f095af309243013953a194b095370d
FINTECHGLOBAL\judhani <judhani@fintechglobal.center>
2018-09-07 14:59:07 +0530

    Data shown properly along with css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { Table } from "antd";
+import 'antd/dist/antd.css';
 const { Column } = Table;
 
 class App extends Component {
@@ -28,18 +29,18 @@ class App extends Component {
     return (
       <div>
         <Table dataSource={movies} rowKey="movie_title">
-          <Column title="Movie" dataIndex="movie" />
-          <Column title="Director" dataIndex="director" />
-          <Column title="Actor 1" dataIndex="actor1" />
-          <Column title="Actor 2" dataIndex="actor2" />
-          <Column title="Genre" dataIndex="genre" />
+          <Column title="Movie" dataIndex="movie_title" />
+          <Column title="Director" dataIndex="director_name" />
+          <Column title="Actor 1" dataIndex="actor_1_name" />
+          <Column title="Actor 2" dataIndex="actor_2_name" />
+          <Column title="Genre" dataIndex="genres" />
           <Column title="Language" dataIndex="language" />
           <Column title="Country" dataIndex="country" />
-          <Column title="Content Rating" dataIndex="rating" />
+          <Column title="Content Rating" dataIndex="content_rating" />
           <Column title="Budget" dataIndex="budget" />
-          <Column title="Year" dataIndex="year" />
-          <Column title="Plot Keywords" dataIndex="plot" />
-          <Column title="IMDB Link" dataIndex="link" />
+          <Column title="Year" dataIndex="title_year" />
+          <Column title="Plot Keywords" dataIndex="plot_keywords" />
+          <Column title="IMDB Link" dataIndex="movie_imdb_link" />
         </Table>
       </div>
     );
